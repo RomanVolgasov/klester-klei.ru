c5a3edba4c111ac1a50c816f02a1ef6fff4509d7
Roman Volgasov <romanvolgasov@gmail.com>
2017-09-26 15:18:13 +0200

    Завёрстано!

--- html/js/custom.js
+++ html/js/custom.js
@@ -54,4 +54,5 @@ $(document).ready(function() {
 });
 
 
-
+(function(){ var widget_id = 'gtx7Ntq4Zr';var d=document;var w=window;function l(){
+      var s = document.createElement('script'); s.type = 'text/javascript'; s.async = true; s.src = '//code.jivosite.com/script/widget/'+widget_id; var ss = document.getElementsByTagName('script')[0]; ss.parentNode.insertBefore(s, ss);}if(d.readyState=='complete'){l();}else{if(w.attachEvent){w.attachEvent('onload',l);}else{w.addEventListener('load',l,false);}}})();
